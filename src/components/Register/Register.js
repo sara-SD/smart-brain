0ba362b98a973dc35b6903bb0a955c9fcc195ada
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2019-06-10 16:43:11 +0430

    changing Register form style

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import '../SignIn/SignIn.css';
 
 class Register extends Component{
     constructor(props){
@@ -50,7 +51,7 @@ class Register extends Component{
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
                                 <input 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="pa2 input-reset ba bg-transparent hover-bg-custom-blue hover-white w-100" 
                                 type="text" 
                                 name="name" 
                                 id="name"
@@ -59,7 +60,7 @@ class Register extends Component{
                             <div className="mt3">
                                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                 <input 
-                                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="pa2 input-reset ba bg-transparent hover-bg-custom-blue hover-white w-100" 
                                 type="email" 
                                 name="email-address"  
                                 id="email-address"
@@ -68,7 +69,7 @@ class Register extends Component{
                             <div className="mv3">
                                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                                 <input 
-                                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                                className="b pa2 input-reset ba bg-transparent hover-bg-custom-blue hover-white w-100" 
                                 type="password" 
                                 name="password"  
                                 id="password"
