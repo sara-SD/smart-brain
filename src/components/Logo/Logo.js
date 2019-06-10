bd15fa40abdc43740e07b19ec619086b6c635fb0
Sara Sadeghian <sara.sadeghian.t@gmail.com>
2019-06-10 16:44:39 +0430

    changing logo and its style

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,9 +7,9 @@ import './Logo.css';
 const Logo = () =>{
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ reverse: true ,max : 60, perspective: 1000, scale: 1.1 , speed: 300 }} style={{ height: 100, width: 100 }} >
                 <div className="Tilt-inner pa2"> 
-                    <img style={{paddingTop: '15px'}} alt='logo' src={brain}/>
+                    <img style={{paddingButtom: '15px'}} alt='logo' src={brain}/>
                 </div>
             </Tilt>
         </div>
